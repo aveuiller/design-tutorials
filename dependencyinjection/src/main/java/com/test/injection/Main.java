37fba86b4546b3d137c46e02f6bd6163cd82b2fd
Antoine Veuiller <aveuiller@online.net>
2019-12-14 13:39:14 +0100

    feat(DI): Add manual injection example

--- dependencyinjection/src/main/java/com/test/injection/Main.java
+++ dependencyinjection/src/main/java/com/test/injection/Main.java
@@ -2,12 +2,22 @@ package com.test.injection;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.test.injection.thermometer.TemperatureUnit;
+import com.test.injection.thermometer.Thermometer;
+import com.test.injection.thermometer.ThermometerContract;
+import com.test.injection.weather.TemperatureStatus;
 import com.test.injection.weather.WeatherContract;
+import com.test.injection.weather.WeatherService;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
 
+    /**
+     * Uses Guice dependency injection module to instantiate
+     * a {@link WeatherContract}.
+     *
+     * @return A {@link WeatherContract} instance
+     */
+    private static WeatherContract injectedWeather() {
         // Creating the Injection module.
         // This module will hold a reference to all the bindings we configured
         // and inject the needed instances as required.
@@ -16,9 +26,28 @@ public class Main {
         // We ask for the injection of WeatherContract, which will create
         // an instance of ThermometerContract with the named TemperatureUnit
         // under the hood.
-        WeatherContract weather = injector.getInstance(WeatherContract.class);
+        return injector.getInstance(WeatherContract.class);
+    }
+
+    /**
+     * This method will do the same thing as injectedWeather
+     * with a manual dependency injection.
+     *
+     * @return A {@link WeatherContract} instance
+     */
+    private static WeatherContract manualWeather() {
+        TemperatureUnit celsius = TemperatureUnit.CELSIUS;
+        ThermometerContract thermometer = new Thermometer(celsius);
+        return new WeatherService(thermometer);
+    }
+
+    public static void main(String[] args) {
+        // Both injectedWeather and manualWeather will have the same behavior.
+        WeatherContract weather = injectedWeather();
+
         for (int i = 0; i < 10; i++) {
-            System.out.println(String.format("The weather is %s", weather.getTemperatureStatus()));
+            TemperatureStatus status = weather.getTemperatureStatus();
+            System.out.println(String.format("The weather is %s", status));
         }
     }
 }
